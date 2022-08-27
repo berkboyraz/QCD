a519a78b0f4a916381ead8424a5174011986d4f4
berkboyraz <56213602+berkboyraz@users.noreply.github.com>
2022-08-27 16:47:32 +0300

    Update LO_phonon_lifetime.m

--- LO_phonon_lifetime.m
+++ LO_phonon_lifetime.m
@@ -4,11 +4,11 @@ e= 1.60217662*10e-19 ; %Unit: Coulomb
 hw_lo=35*10e-3*e ; % Unit: Joule
 h_bar= 1.0545718*10e-34 ; % Unit: Joule.s
 w_lo=hw_lo/h_bar;
-eps_rho=69; % Unit: F.m^-1
+eps_rho=69; % Unit: F.m^-1, Bundan emin değilim
 
 
 k_l= sqrt(((hw_lo-(E_u-E_l))*2*m_l)/(h_bar^2));
 
 I=;
 
-t_ij_inverse= (m_l*e^2*w_lo*I)/(4*h_bar^2*eps_rho^-1);
+t_ij_inverse= (m_l*e^2*w_lo*I)/(4*h_bar^2*eps_rho);
